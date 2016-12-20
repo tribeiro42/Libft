95576099712a73da1a3ae50b479e85f31b0d19b0
Tristan RIBEIRO <tribeiro@e1r1p8.42.fr>
2016-12-20 16:50:34 +0100

    [+] Function lst sort content

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: tribeiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/18 04:05:46 by tribeiro          #+#    #+#             */
-/*   Updated: 2016/12/13 14:47:15 by tribeiro         ###   ########.fr       */
+/*   Updated: 2016/12/20 16:50:10 by tribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,4 +112,5 @@ void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+void				ft_lst_sort_content(t_list *lst, (*cmp)());
 #endif
---
+++ srcs/lst/ft_lst_sort_content.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tribeiro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/13 16:54:44 by tribeiro          #+#    #+#             */
+/*   Updated: 2016/12/20 16:48:50 by tribeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		ft_lst_sort(t_list **lst, int (*cmp)())
+{
+	t_list		*begin_lst;
+	t_list		*elem;
+	t_list		*swap;
+
+	if(!(*lst))
+		(*lst) = NULL;
+	begin_lst = *lst;
+	while(*lst != NULL)
+	{
+		elem = begin_lst;
+		while(elem->next)
+		{
+			if(cmp(elem->content, elem->next->content) > 0)
+			{
+				swap = elem->content;
+				elem->content = elem->next->content;
+				elem->next->content = swap;
+			}
+			elem = elem->next;
+		}
+		*lst = (*lst)->next;
+	}
+	*lst = begin_lst;
+}
